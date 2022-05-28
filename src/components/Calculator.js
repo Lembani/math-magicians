582c160483bdf58a31d5193c4003db2eeaa8e586
Lembani Sakala <lembanisakala@gmail.com>
2022-05-28 14:25:24 +0200

    Update Calculator componenent: - use functional components - use hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,64 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import calculate from '../logic/calculate';
 import Button from './Button';
 
-class Calculator extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = { total: 0, next: null, operation: null };
-  }
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
 
-  componentDidMount = () => {
-    this.setState({
-      total: 0,
-      next: null,
-      operation: null,
-    });
+  const handleChange = (e) => {
+    const oprObject = calculate(state, e.target.textContent);
+    setState(oprObject);
   };
 
-  render() {
-    const handleChange = (e) => {
-      const oprObject = calculate(this.state, e.target.textContent);
-      this.setState(oprObject);
-    };
-
-    const { total, operation, next } = this.state;
-    const opr = operation === '%' ? 'mod' : operation;
-    let result = '';
-    if (total) {
-      result = `${total} ${opr || ''} ${next || ''}`;
-    } else if (next) {
-      result = `${next} ${opr || ''}`;
-    }
-
-    return (
-      <>
-        <div className="main-calculator">
-          <div className="cell result">{ result || 0 }</div>
-          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="AC" />
-          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="+/-" />
-          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="%" />
-          <Button handleChange={(e) => handleChange(e)} styleClass="cell ops" val="÷" />
-          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="7" />
-          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="8" />
-          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="9" />
-          <Button handleChange={(e) => handleChange(e)} styleClass="cell ops" val="x" />
-          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="4" />
-          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="5" />
-          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="6" />
-          <Button handleChange={(e) => handleChange(e)} styleClass="cell ops" val="-" />
-          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="1" />
-          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="2" />
-          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="3" />
-          <Button handleChange={(e) => handleChange(e)} styleClass="cell ops" val="+" />
-          <Button handleChange={(e) => handleChange(e)} styleClass="cell zero" val="0" />
-          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="." />
-          <Button handleChange={(e) => handleChange(e)} styleClass="cell ops" val="=" />
-        </div>
-      </>
-    );
+  const { total, operation, next } = state;
+  const opr = operation === '%' ? 'mod' : operation;
+  let result = '';
+  if (total) {
+    result = `${total} ${opr || ''} ${next || ''}`;
+  } else if (next) {
+    result = `${next} ${opr || ''}`;
   }
-}
+
+  return (
+    <>
+      <div className="main-calculator">
+        <div className="cell result">{ result || 0 }</div>
+        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="AC" />
+        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="+/-" />
+        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="%" />
+        <Button handleChange={(e) => handleChange(e)} styleClass="cell ops" val="÷" />
+        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="7" />
+        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="8" />
+        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="9" />
+        <Button handleChange={(e) => handleChange(e)} styleClass="cell ops" val="x" />
+        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="4" />
+        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="5" />
+        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="6" />
+        <Button handleChange={(e) => handleChange(e)} styleClass="cell ops" val="-" />
+        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="1" />
+        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="2" />
+        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="3" />
+        <Button handleChange={(e) => handleChange(e)} styleClass="cell ops" val="+" />
+        <Button handleChange={(e) => handleChange(e)} styleClass="cell zero" val="0" />
+        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="." />
+        <Button handleChange={(e) => handleChange(e)} styleClass="cell ops" val="=" />
+      </div>
+    </>
+  );
+};
 
 export default Calculator;
