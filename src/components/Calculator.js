07df2921645276a72aa5ade0bd729b7611fc6cac
Lembani Sakala <lembanisakala@gmail.com>
2022-06-01 19:41:40 +0200

    Update Calculator component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import './Calculator.css';
+
 import calculate from '../logic/calculate';
 import Button from './Button';
 
@@ -26,27 +28,30 @@ const Calculator = () => {
 
   return (
     <>
-      <div className="main-calculator">
-        <div className="cell result">{ result || 0 }</div>
-        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="AC" />
-        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="+/-" />
-        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="%" />
-        <Button handleChange={(e) => handleChange(e)} styleClass="cell ops" val="÷" />
-        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="7" />
-        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="8" />
-        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="9" />
-        <Button handleChange={(e) => handleChange(e)} styleClass="cell ops" val="x" />
-        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="4" />
-        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="5" />
-        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="6" />
-        <Button handleChange={(e) => handleChange(e)} styleClass="cell ops" val="-" />
-        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="1" />
-        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="2" />
-        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="3" />
-        <Button handleChange={(e) => handleChange(e)} styleClass="cell ops" val="+" />
-        <Button handleChange={(e) => handleChange(e)} styleClass="cell zero" val="0" />
-        <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="." />
-        <Button handleChange={(e) => handleChange(e)} styleClass="cell ops" val="=" />
+      <div className="calculator-container">
+        <h2 className="calculator-heading">Let&apos;s do some math!</h2>
+        <div className="main-calculator">
+          <div className="cell result">{ result || 0 }</div>
+          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="AC" />
+          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="+/-" />
+          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="%" />
+          <Button handleChange={(e) => handleChange(e)} styleClass="cell ops" val="÷" />
+          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="7" />
+          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="8" />
+          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="9" />
+          <Button handleChange={(e) => handleChange(e)} styleClass="cell ops" val="x" />
+          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="4" />
+          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="5" />
+          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="6" />
+          <Button handleChange={(e) => handleChange(e)} styleClass="cell ops" val="-" />
+          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="1" />
+          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="2" />
+          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="3" />
+          <Button handleChange={(e) => handleChange(e)} styleClass="cell ops" val="+" />
+          <Button handleChange={(e) => handleChange(e)} styleClass="cell zero" val="0" />
+          <Button handleChange={(e) => handleChange(e)} styleClass="cell" val="." />
+          <Button handleChange={(e) => handleChange(e)} styleClass="cell ops" val="=" />
+        </div>
       </div>
     </>
   );
