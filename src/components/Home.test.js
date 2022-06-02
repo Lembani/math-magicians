9488a3c2a097823692781e33337d6cd9f9ff4cd6
Lembani Sakala <lembanisakala@gmail.com>
2022-06-02 13:45:36 +0200

    Fix linter errors

--- src/components/Home.test.js
+++ src/components/Home.test.js
@@ -3,24 +3,24 @@ import renderer from 'react-test-renderer';
 import Home from './Home';
 
 test('heading', () => {
-    render(<Home />);
-    const heading = screen.getByRole('heading');
-    expect(heading).toBeInTheDocument();
+  render(<Home />);
+  const heading = screen.getByRole('heading');
+  expect(heading).toBeInTheDocument();
 });
 
 test('First paragraph', () => {
-    render(<Home />)
-    const paragraphs = document.querySelector('.para-one');
-    expect(paragraphs).toBeInTheDocument();
+  render(<Home />);
+  const paragraphs = document.querySelector('.para-one');
+  expect(paragraphs).toBeInTheDocument();
 });
 
 test('Second paragraph', () => {
-    render(<Home />)
-    const paragraphs = document.querySelector('.para-two');
-    expect(paragraphs).toBeInTheDocument();
+  render(<Home />);
+  const paragraphs = document.querySelector('.para-two');
+  expect(paragraphs).toBeInTheDocument();
 });
 
 it('It renders correctly', () => {
-    const tree = renderer.create(<Home />).toJSON()
-    expect(tree).toMatchSnapshot();
-})
+  const tree = renderer.create(<Home />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
