8f15d9f5d7c9f8b7757d6269e337416573b1ad14
Lembani Sakala <lembanisakala@gmail.com>
2022-06-02 13:45:24 +0200

    Add Snapshot to Button

--- src/components/Button.test.js
+++ src/components/Button.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import Button from './Button';
 
-test('renders button to the screen', () => {
+test('Renders `Button` to the screen', () => {
   render(<Button />);
   const btnElement = screen.getByRole('button');
   expect(btnElement).toBeInTheDocument();
+});
+
+it('Should render `Button` correctly', () => {
+  const tree = renderer.create(<Button />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
