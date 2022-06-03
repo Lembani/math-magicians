a25ee9224253c3f257da1c7a06f6d79ec821658d
Lembani Sakala <lembanisakala@gmail.com>
2022-06-03 11:10:59 +0200

    Add test if number is decimal

--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -12,3 +12,18 @@ describe('Number has been entered', () => {
     expect(result.next).toEqual('26');
   });
 });
+
+describe('Number entered is decimal', () => {
+  test('If dot (.) is pressed', () => {
+    let btn = '2';
+    let result = calculate({
+      total: null, next: null, operation: null,
+    }, btn);
+    expect(result.next).toEqual('2');
+    btn = '.';
+    result = calculate(result, btn);
+    btn = '5';
+    result = calculate(result, btn);
+    expect(result.next).toEqual('2.5');
+  });
+});
