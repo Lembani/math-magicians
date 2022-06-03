faf196d6286d55ce1580cae263b5f53ad36a0b1c
Dinma-Faith <phaytep@gmail.com>
2022-06-03 10:40:26 +0100

    Add test for addition in operate.test

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -4,3 +4,9 @@ test('multiplication', () => {
     const result = operate('5', '2', 'x');
     expect(result).toBe('10');
 });
+
+test('addition', () => {
+    const result = operate('2', '3', '+');
+    expect(result).toBe('5');
+});
+
