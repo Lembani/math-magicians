a15f9e387d4dcfaf50e9871c8a907b3ba8d88cca
Lembani Sakala <lembanisakala@gmail.com>
2022-06-03 16:06:40 +0200

    Fix linter errors

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -1,26 +1,26 @@
 import operate from './operate';
 
 test('multiplication', () => {
-    const result = operate('5', '2', 'x');
-    expect(result).toBe('10');
+  const result = operate('5', '2', 'x');
+  expect(result).toBe('10');
 });
 
 test('addition', () => {
-    const result = operate('2', '3', '+');
-    expect(result).toBe('5');
+  const result = operate('2', '3', '+');
+  expect(result).toBe('5');
 });
 
 test('subtraction', () => {
-    const result = operate('7', '4', '-');
-    expect(result).toBe('3');
+  const result = operate('7', '4', '-');
+  expect(result).toBe('3');
 });
 
 test('division', () => {
-    const result = operate('10', '5', '÷');
-    expect(result).toBe('2');
+  const result = operate('10', '5', '÷');
+  expect(result).toBe('2');
 });
 
 test('modulos', () => {
-    const result = operate('10', '5', '%');
-    expect(result).toBe('0');
+  const result = operate('10', '5', '%');
+  expect(result).toBe('0');
 });
